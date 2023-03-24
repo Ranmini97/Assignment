f66276496906495683ba1653e81bfe27d5cdfba9
Ranmini <ranminierandika3@gmail.com>
2023-03-24 10:53:43 +0530

    added event class - modified

--- src/main/java/com/randoli/assignment/entity/Event.java
+++ src/main/java/com/randoli/assignment/entity/Event.java
@@ -3,12 +3,10 @@ package com.randoli.assignment.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-<<<<<<< HEAD
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-=======
->>>>>>> origin/master
 //import jakarta.persistence.TemporalType;
 //import org.springframework.data.jpa.repository.Temporal;
 
@@ -16,14 +14,9 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-<<<<<<< HEAD
 @NoArgsConstructor
 @AllArgsConstructor
 @Data //to add getters and setters
-=======
-
- //to add getters and setters
->>>>>>> origin/master
 public class Event {
     @Id
     @Column(name = "event_id", columnDefinition = "BINARY(16)") //UUID have fixed length of 16 bytes (stores 126-bit value in binary format)
@@ -34,30 +27,17 @@ public class Event {
     private LocalDateTime transTms;
     @Column(name = "re_num", columnDefinition = "VARCHAR(10)")
     private String reNum;
-<<<<<<< HEAD
-=======
-
-
->>>>>>> origin/master
     @Column(name = "client_id", columnDefinition = "VARCHAR(15)")
     private String clientId;
     @Column(name = "event_cnt", columnDefinition = "INT")
     private int eventCnt;
     @Column(name = "location_cd", columnDefinition = "VARCHAR(15)")
     private String locationCd;
-<<<<<<< HEAD
     @Column(name = "location_id1", columnDefinition = "VARCHAR(10)")
     private String locationId1;
     @Column(name = "location_id2", columnDefinition = "VARCHAR(10)")
     private String locationId2;
     @Column(name = "addr_nbr", columnDefinition = "VARCHAR(15)")
-=======
-    @Column(name = "trans_tms", columnDefinition = "VARCHAR(10)")
-    private String locationId1;
-    @Column(name = "trans_tms", columnDefinition = "VARCHAR(10)")
-    private String locationId2;
-    @Column(name = "trans_tms", columnDefinition = "VARCHAR(15)")
->>>>>>> origin/master
     private String addrNbr;
 
 
